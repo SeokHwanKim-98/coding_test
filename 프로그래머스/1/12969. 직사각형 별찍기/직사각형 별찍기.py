912948be5a3a2f24d4ee4f8c83ed913a544193e4
SeokHwanKim-98 <133570351+SeokHwanKim-98@users.noreply.github.com>
2026-07-16 22:05:11 +0900

    Fix newline and add comments in rectangle star printing
    Added comments for clarification and ensured proper newline at the end of the file.

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.py
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.py
@@ -4,3 +4,7 @@ for i in range(b) :
     for j in range(a) :
         print("*",end="")
     print()
+
+# map?이 뭔지 모르겠음, 뒤는 정수, 입력받음().strip()?.공백(' ')으로 분류됨 인거까진 알겠음
+
+#jupyter notebook 에 위 코드 복사 후 예시 집어넣어서 입력 순서대로 들어오는것만 파악하고 풀이함
